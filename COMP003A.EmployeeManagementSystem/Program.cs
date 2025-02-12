4c9c02ccb6bb2905183f746ed09fba44d6e0e444
flores-97 <floresvictor15@gmail.com>
2025-02-12 08:01:26 -0800

    Add project files.

---
+++ COMP003A.EmployeeManagementSystem/Program.cs
@@ -0,0 +1,14 @@
+﻿//Author: Victor Flores
+//Course: COMP-003
+//Faculty: Jonathon Cruz
+//Purpose: Employee management system demonstrating OOP principles in C#
+namespace COMP003A.EmployeeManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
